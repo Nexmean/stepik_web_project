985f2dd942cf45d7c55a0d309b4d6dd25228a96f
Alexander Makarov <next.meaning@gmail.com>
2018-08-08 22:57:04 +1000

    Второе задание на stepik

---
+++ hello.py
@@ -0,0 +1,9 @@
+from urlparse import parse_qs
+
+
+def app(environ, start_response):
+    qs = parse_qs(environ['QUERY_STRING'])
+    start_response('200 OK', [
+        ('Content-Type', 'text/plain')
+    ])
+    return ['{}={}'.format(key, value) for key, value in qs.items()]
